04b4401e41e4fc51e60268c8807350c7391c57d8
Varad Ahirwadkar <varad.ahirwadkar1@ibm.com>
2026-09-21 11:58:50 +0530

    Set route targetPort to http in OGX test fixture
    Signed-off-by: Varad Ahirwadkar <varad.ahirwadkar1@ibm.com>

--- tests/ogx/conftest.py
+++ tests/ogx/conftest.py
@@ -255,7 +255,8 @@ def ogx_test_route(
                             "tls": {
                                 "termination": "edge",
                                 "insecureEdgeTerminationPolicy": "Redirect",
-                            }
+                            },
+                            "port": {"targetPort": "http"},
                         },
                         "metadata": {
                             "annotations": {Annotations.HaproxyRouterOpenshiftIo.TIMEOUT: "10m"},
